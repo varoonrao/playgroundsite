5daa3bef309f7972261e9a274a5e0b7272a1f132
varoonrao <varunrao72@gmail.com>
2019-01-21 18:59:26 +0530

    success

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>playground</title>
+		<style type="text/css">
+			.header{
+				width:100%;
+				height:10%;
+				color: white;
+				background-color:blue;
+			}
+			.head_text{
+				color:white;
+				margin-left: 20%;
+				margin-top: 3px;
+				padding-top: 3px;
+				font-size: 20px;
+			}
+
+		</style>
+	</head>
+
+	<body style="font-family: product sans">
+		<div class="header">
+			<p class="head_text">Playground Booking</p>
+		</div>
+		<div class="sidenav">
+			<ul>
+				<li>About Us</li>
+				<li>Contact Us</li>
+			</ul>
+		</div>
+	</body>
+</html>
